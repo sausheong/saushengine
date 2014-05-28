ba2fb9e286a475e4ab1212062e751dee14925361
Chang Sau Sheong <sausheong@gmail.com>
2014-05-29 00:50:48 +0800

    extracting words

--- tests/spider_tests.rb
+++ tests/spider_tests.rb
@@ -5,9 +5,12 @@ require './worker'
 TEST_URLS = [
   "http://www.ndtv.com/article/world/barack-obama-leaves-afghanistan-after-surprise-troop-visit-530672",
   "http://www.詹姆斯.com/",
+  "https://sg.news.yahoo.com/blogs/fit-to-post-autos/brain-cancer-survivor-shawn-low-starts-dream-drive-023034432.html",
   "http://www.adobe.com/enterprise/accessibility/pdfs/acro6_pg_ue.pdf",
   "http://media.npr.org/assets/img/2012/02/02/mona-lisa_custom-31a0453b88a2ebcb12c652bce5a1e9c35730a132-s6-c30.jpg"
 ]
+
+HTML_URLS = TEST_URLS[0..2]
 
 TEST_MIME_TYPES = [
   "text/html",
@@ -31,5 +34,11 @@ class TestSpider < MiniTest::Unit::TestCase
     end        
   end
 
+  def test_extract_all_words
+    HTML_URLS.each do |url|
+      refute_nil extract_all_words(normalize(url).to_s) 
+    end    
+    
+  end
   
 end
